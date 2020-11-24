87504aa5a02a646858cd67bbc70a79f0b94e06b2
marec <827878579@qq.com>
2020-11-24 14:55:59 +0800

    上传vue cli文件

---
+++ src/views/布局/Components/BoxComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ boxclass }}</h1>
+    <div :class="boxclass" style="display: flex">
+      <div class="red"></div>
+      <div class="blue"></div>
+      <div class="purple"></div>
+      <div class="orange"></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "boxcomponent",
+  data() {
+    return {};
+  },
+  props: ["boxclass"],
+};
+</script>
+
+<style>
+.red {
+  height: 100px;
+  width: 100px;
+  background-color: #1abc9c;
+}
+.blue {
+  height: 150px;
+  width: 150px;
+  background-color: #2ecc71;
+}
+.purple {
+  height: 200px;
+  width: 200px;
+  background-color: #3498db;
+}
+.orange{
+  height: 250px;
+  width: 250px;
+  background-color: #9b59b6;
+}
+</style>
---
+++ src/views/布局/flex.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div class="boxcontainer1">
+      <titleComponent :titleText="titleText"></titleComponent>
+      <boxcomponent
+        v-for="n in 20"
+        :key="n"
+        :boxclass="'box' + n"
+      ></boxcomponent>
+    </div>
+    <h1>boxes</h1>
+    <div class="boxcontainer2">
+      <numberedBoxComponent
+        :numberText="n"
+        v-for="n in 15"
+        class="box"
+        :key="n"
+      ></numberedBoxComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import boxcomponent from "./Components/BoxComponent"; // ../上一级目录下
+import titleComponent from "./Components/titleComponent"; // ./ 当前目录下
+import numberedBoxComponent from "./Components/numberedBoxComponent";
+
+export default {
+  components: {
+    boxcomponent,
+    titleComponent,
+    numberedBoxComponent,
+  },
+  data() {
+    return {
+      titleText: "flex布局",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.titleOfItems {
+  text-align: 20px;
+}
+/* 主要是由justify-content\align-items\align-content(wrap)\align-items(单个元素特例)四种 */
+.boxcontainer1 {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.boxcontainer2 {
+  background-color: #16a085;
+  /* 不设置height由内部元素撑起大小 */
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  /* align-content只在多行显示为flex-wrap时有效 */
+  /* space-aroud两行间距更小,space-between两行间距较大 */
+  align-content: space-around;
+}
+.box {
+  height: 100px;
+  width: 100px;
+  background-color: #1abc9c;
+  margin: 20px;
+}
+/* justify-content针对元素在列上的分布 */
+.box1 {
+  display: flex;
+}
+.box2 {
+  display: flex;
+  justify-content: center;
+}
+.box3 {
+  display: flex;
+  justify-content: flex-end;
+  /* end 对 justify-content无效 */
+}
+.box4 {
+  display: flex;
+  justify-content: space-around;
+}
+.box5 {
+  display: flex;
+  justify-content: space-between;
+}
+.box6 {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+/* align-items针对元素在行上的分布 */
+.box7 {
+  display: flex;
+  align-items: flex-start;
+}
+.box8 {
+  display: flex;
+  align-items: center;
+}
+.box9 {
+  display: flex;
+  align-items: flex-end;
+}
+.box10 {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.box11 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.box12 {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
